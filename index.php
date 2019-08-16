0d001764cbb5b8b23c2b3406681f53f9ebe8ec2f
Colin Wagaba <ckwagaba@gmail.com>
2019-08-16 22:31:14 +0300

    add input elements

---
+++ index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Customer Information</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="./images/favicon.png" />
+    <link rel="stylesheet" type="text/css" media="screen" href="./css/main.css">
+    <link rel="stylesheet" type="text/css" media="screen" href="./css/res.css">
+    <script src="./js/main.js"></script>
+</head>
+<body>
+    <header>
+        <div class="wrapper">
+            <div class="logo-container"><img src="./images/logo.png" /></div>
+            <div class="title">
+                <div class="heading">Join our REF. newsletters</div>
+                <div class="sub-heading">Sign up to receive a FLAT 10% off your ORDER NOW.</div>
+            </div>
+        </div>
+    </header>
+    <main>
+        <div class="wrapper">
+            <div class="form-wrapper">
+                <form action="" method="POST">
+                    <div class="input-wrapper">
+                        <div class="input-group">
+                            <div class="input-label">First Name</div>
+                            <div class="input-field">
+                                <input type="text" />
+                            </div>
+                        </div>
+                        <div class="input-group">
+                            <div class="input-label">Last Name</div>
+                            <div class="input-field">
+                                <input type="text" />
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="input-wrapper">
+                        <div class="input-group only-child">
+                            <div class="input-label">Email Address</div>
+                            <div class="input-field">
+                                <input type="text" />
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="input-wrapper">
+                        <div class="input-group">
+                            <div class="input-label">Phone Number</div>
+                            <div class="input-field">
+                                <input type="text" />
+                            </div>
+                        </div>
+                        <div class="input-group">
+                            <div class="input-label">Date of Birth</div>
+                            <div class="input-field">
+                                <input type="text" />
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="input-wrapper">
+                        <div class="input-group only-child">
+                            <div class="input-label">Country of Residence</div>
+                            <div class="input-field">
+                                <input type="text" />
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </main>
+    <footer>
+        <div class="wrapper"></div>
+    </footer>
+</body>
+</html>
