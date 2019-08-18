ca858fe53d1a952495c5665e5b9c6709304caabe
Colin Wagaba <ckwagaba@gmail.com>
2019-08-18 08:12:12 +0300

    add country dial codes

--- includes/countries.php
+++ includes/countries.php
@@ -6,9 +6,9 @@
         <?php
         $countries_string = file_get_contents('./json/countries.json');
         $countries_array = json_decode($countries_string, true);
-        foreach ($countries_array as $key => $value) {
-            $item = '<option class="md-select-component-item" value="' . $value . '">';
-                $item .= $value;
+        foreach ($countries_array as $country) {
+            $item = '<option class="md-select-component-item" value="' . $country['name'] . '" data-code="' . $country['code'] . '" data-dial-code="' . $country['dial_code'] . '">';
+                $item .= $country['name'];
             $item .= '</option>';
             echo $item;
         }
