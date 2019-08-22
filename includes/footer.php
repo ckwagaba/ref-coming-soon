2827f63699b1e4f176a0978fa2af9e7b8e96bf9f
Colin Wagaba <ckwagaba@gmail.com>
2019-08-22 12:09:29 +0300

    add social links

--- includes/footer.php
+++ includes/footer.php
@@ -1,11 +1,9 @@
 <footer>
         <div class="wrapper">
             <div class="social">
-                <?php
-                echo file_get_contents("./images/facebook.svg");
-                echo file_get_contents("./images/instagram.svg");
-                echo file_get_contents("./images/youtube.svg");
-                ?>
+                <a href="https://www.facebook.com/reflivingraw2/" target="_NEW"><?php echo file_get_contents("./images/facebook.svg"); ?></a>
+                <a href="https://www.instagram.com/ref.cafe/" target="_NEW"><?php echo file_get_contents("./images/instagram.svg"); ?></a>
+                <a href="https://www.youtube.com/channel/UCL6vw_0BQubT141aqvzlBWw" target="_NEW"><?php echo file_get_contents("./images/youtube.svg"); ?></a>
             </div>
             <div class="links">
                 <?php
