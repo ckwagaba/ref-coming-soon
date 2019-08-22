11ea9680cf3f11871e55ad95825d20dbf6bc0bdb
Colin Wagaba <ckwagaba@gmail.com>
2019-08-22 13:19:25 +0300

    add tripadvisor logo

--- includes/footer.php
+++ includes/footer.php
@@ -4,6 +4,7 @@
                 <a href="https://www.facebook.com/reflivingraw2/" target="_NEW"><?php echo file_get_contents("./images/facebook.svg"); ?></a>
                 <a href="https://www.instagram.com/ref.cafe/" target="_NEW"><?php echo file_get_contents("./images/instagram.svg"); ?></a>
                 <a href="https://www.youtube.com/channel/UCL6vw_0BQubT141aqvzlBWw" target="_NEW"><?php echo file_get_contents("./images/youtube.svg"); ?></a>
+                <a href="https://www.tripadvisor.co.uk/Restaurant_Review-g293841-d15613512-Reviews-Ref_Cafe-Kampala_Central_Region.html?m=19905" target="_NEW"><?php echo file_get_contents("./images/tripadvisor.svg"); ?></a>
             </div>
             <div class="links">
                 <?php
