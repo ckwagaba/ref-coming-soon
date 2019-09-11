4f1aeeb920d9db38937b8d1615b50fe0d1ebba44
Colin Wagaba <ckwagaba@gmail.com>
2019-09-11 08:11:13 +0300

    update landing page

--- includes/meta.php
+++ includes/meta.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <title>Customer Information</title>
+    <title>Coming Soon</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" href="./images/favicon.png" />
     <link rel="stylesheet" type="text/css" media="screen" href="./css/main.css">
--- index.php
+++ index.php
@@ -1,93 +1,38 @@
 <?php
 require_once('./includes/meta.php');
 ?>
-<div id="home-page">
-<header>
-    <div class="wrapper">
+<div id="hero">
+    <div>
+        <?php
+        echo file_get_contents("./images/logo-arrow.svg");
+        ?>
+    </div>
+</div>
+<div id="main">
+    <header>
         <div class="logo-container">
             <?php echo file_get_contents("./images/logo.svg"); ?>
         </div>
         <div class="title">
-            <div class="heading">Join our REF. newsletters</div>
-            <div class="sub-heading">Sign up to receive a FLAT <strong>10% off</strong> your ORDER NOW.</div>
+            <div class="sub-heading">Stay Tuned</div>
+            <div class="heading">We are launching soon!</div>
+            <div class="main-footer-copy">
+                Be notified first when the REF. website goes live. <br />Get a <strong>FLAT 15%</strong> Discount Shopping Voucher as our Loyal VIP Customer.
+            </div>
         </div>
-    </div>
-</header>
-<main>
-    <div class="wrapper">
+    </header>
+    <main>
         <div class="form-wrapper">
             <form action="" method="POST">
-                <div class="input-wrapper">
-                    <div class="input-group">
-                        <div class="input-label">First Name *</div>
-                        <div class="input-field">
-                            <input type="text" placeholder="Jane" id="first-name" required />
-                        </div>
-                    </div>
-                    <div class="input-group">
-                        <div class="input-label">Last Name *</div>
-                        <div class="input-field">
-                            <input type="text" placeholder="Doe" id="last-name" required />
-                        </div>
-                    </div>
-                </div>
 
                 <div class="input-wrapper">
-                    <div class="input-group only-child">
+                    <div class="input-group">
                         <div class="input-label">Email Address *</div>
                         <div class="input-field">
                             <input type="email" placeholder="example@domain.com" id="email" required />
+                            <button id="submit-button">Notify Me</button>
                         </div>
                     </div>
-                </div>
-
-                <div class="input-wrapper">
-                    <div class="input-group">
-                        <div class="input-label">Country of Residence</div>
-                        <div class="select-group">
-                            <?php require_once('./includes/countries.php'); ?>
-                        </div>
-                    </div>
-                    <div class="input-group">
-                        <div class="input-label">Phone Number</div>
-                        <div class="input-field">
-                            <input type="tel" placeholder="+256 700 123456" id="phone" />
-                        </div>
-                    </div>
-                </div>
-
-                <div class="input-wrapper">
-                    <div class="input-group only-child">
-                        <div class="input-label">Date of Birth</div>
-                        <div class="select-group">
-                            <?php require_once('./includes/months.php'); ?>
-                            <?php require_once('./includes/days.php'); ?>
-                            <?php require_once('./includes/years.php'); ?>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="input-wrapper">
-                    <div class="input-group only-child">
-                        <div class="checkbox-wrapper">
-                            <div class="checkbox">
-                                <input type="checkbox" id="cafe-subscription" />
-                                <div class="check-mark-wrapper"><?php echo file_get_contents("./images/check.svg"); ?></div>
-                            </div>
-                            <div class="checkbox-label">Subscribe to <strong>REF. CAF&Eacute;</strong> newsletters.</div>
-                        </div>
-                        <div class="checkbox-wrapper">
-                            <div class="checkbox">
-                                <input type="checkbox" id="sports-subscription" />
-                                <div class="check-mark-wrapper"><?php echo file_get_contents("./images/check.svg"); ?></div>
-                            </div>
-                            <div class="checkbox-label">Subscribe to <strong>REF. SPORTS</strong> newsletters.</div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="button-wrapper">
-                    <button id="submit-button">submit</button>
                 </div>
 
                 <div id="form-feedback">&nbsp;</div>
@@ -95,9 +40,6 @@ require_once('./includes/meta.php');
         </div>
 
         <div class="main-footer">
-            <div class="main-footer-copy">
-                Be notified first when the REF. website goes live. <br />Get a <strong>FLAT 15%</strong> Discount Shopping Voucher as our Loyal VIP Customer.
-            </div>
             <div class="main-footer-logos">
                 <?php
                 echo file_get_contents("./images/cafe.svg");
@@ -106,9 +48,5 @@ require_once('./includes/meta.php');
                 ?>
             </div>
         </div>
-    </div>
-</main>
+    </main>
 </div>
-<?php
-require_once('./includes/footer.php');
-?>
